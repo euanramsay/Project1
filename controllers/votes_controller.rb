969bb8f3d68682bd783753584a66b9a43ab17780
euanramsay <euancramsay@aol.com>
2016-10-25 17:57:54 +0100

    Added rules for not allowing double votes and voting for yourself

--- controllers/votes_controller.rb
+++ controllers/votes_controller.rb
@@ -20,14 +20,45 @@ end
 post '/votes' do
   @vote = Vote.new(params)
   @votes = Vote.all
-  @voted = false
+  voted = false
+  double_vote = false
+  self_vote = false
+  first_vote = params["vote1_id"].to_i
+  second_vote = params["vote2_id"].to_i
+  third_vote = params["vote3_id"].to_i
   pub_id_number = params["pub_id"].to_i
-    @votes.each do |vote|
-      if vote.pub_id == pub_id_number
-        @voted = true
-      end
+    
+  # decide if a pub has already voted
+
+  @votes.each do |vote|
+    if vote.pub_id == pub_id_number
+      voted = true
     end
-  if @voted == true
+  end
+
+  # decide if a pub has voted for the same pub more than once
+
+  if first_vote == second_vote || first_vote == third_vote
+    double_vote = true
+  elsif second_vote == third_vote
+    double_vote = true
+  end
+
+  # decide if a pub has voted for itself
+
+  if first_vote == pub_id_number || second_vote == pub_id_number
+    self_vote = true
+  elsif third_vote == pub_id_number
+    self_vote = true
+  end
+
+  # redirect user to error message or save vote
+
+  if voted == true
+    redirect to("/")
+  elsif double_vote == true
+    redirect to("/")
+  elsif self_vote
     redirect to("/")
   else
     @vote.save
